ce64ecb71573d5952359d475ebbc19f1ad35fe1f
Sriram <166107943+Ram11182089@users.noreply.github.com>
2024-07-09 20:18:20 +0530

    Add files via upload

---
+++ 9_funtions.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    console.log(a+b);
+}
+
+add(2,5);
+add(27,5);
+add(2,57);
+
+function sub(a,b){
+    return a-b
+}
+ let r=sub(9,7);
+ console.log(r)
+
+let func =(x)=>{
+    console.log(x)
+}
+
+func(8);
+func(7)
